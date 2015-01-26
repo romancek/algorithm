e48d1dfa5d817d3b7e343d2ce32aa80636d9113e
lab.pc <s.y2kmi@gmail.com>
2015-01-26 13:40:18 +0900

    few changes

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -4,10 +4,11 @@
 #define N 10	//切り分けパターンの数
 #define L 10	//もともとの金属棒の長さ
 
-void setArrays(void);
+void setArrays(int);
 void freeArrays(void);
 void solve(void);
 void backtrack(int,int,int);
+int max(int,int);
 void print_result(void);
 
 struct InputSet{
@@ -15,13 +16,11 @@ struct InputSet{
 	int price;
 };
 
-struct InputSet s[N+1] =
-	 {{0,0},{1,1},{2,5},{3,8},{4,9},{5,10},{6,17},{7,17},{8,20},{9,24},{10,30}};
-struct InputSet *ms = NULL;	//個数制限無しのナップサック問題用に，修正した入力をここに保存
+struct InputSet inputs[N] =
+	 {{1,1},{2,5},{3,8},{4,9},{5,10},{6,17},{7,17},{8,20},{9,24},{10,30}};
 int **c = NULL;	//c[i][l]は長さlの金属棒についてi番目の切り分けパターンまで考慮したときの価格の最大値
 int result[N+1] = {0};
 int length = L;
-int newsize;
 
 int main(int argc,char *argv[])
 {
@@ -29,9 +28,9 @@ int main(int argc,char *argv[])
 	while(length > 0){
 		for(int i = 0;i <= length+1;i++)printf("____ ");
 		printf("\nL = %2d : result \n",length);
-		setArrays();
+		setArrays(1);
 		solve();
-		backtrack(newsize,length,0);
+		backtrack(N,length,0);
 		print_result();
 		freeArrays();
 		length--;
@@ -39,37 +38,17 @@ int main(int argc,char *argv[])
 	return 0;
 }
 
-void setArrays()
+void setArrays(int change)
 {
-	newsize = 0;
-	for(int i = 1;i <= N;i++)newsize += (int)(length/s[i].length);
-	ms = (struct InputSet *)malloc(sizeof(struct InputSet) * (newsize + 1));
-	printf("NEWSIZE = %3d\n",newsize);
-	ms[0] = {0,0};
-
-	int currentIndex = 1;
-	for(int i = 1;i <= N;i++){
-		int count = (int)(length / s[i].length);
-		for(int x = 0;x < count;x++)ms[x+currentIndex] = {s[i].length,s[i].price};
-		currentIndex += count;
-	}
-	printf("MODIFIED INPUT IS \n[ ");
-	for(int i = 0;i <= newsize;i++){
-		printf("{%2d,%2d} ",ms[i].length,ms[i].price);
-		if(i % length == 0)printf("\n");
-	}
-	printf("]\n\n");
-	
-	c = (int **)malloc(sizeof(int *) * (newsize + 1));
-	for(int i = 0;i <= newsize;i++){
+	c = (int **)malloc(sizeof(int *) * (N + 1));
+	for(int i = 0;i <= N;i++){
 		c[i] = (int *)malloc(sizeof(int) * (length + 1));
 	}
 }
 
 void freeArrays()
 {
-	free(ms);
-	for(int i = 0;i <= newsize;i++){
+	for(int i = 0;i <= N;i++){
 		free(c[i]);
 	}
 	free(c);
@@ -77,22 +56,19 @@ void freeArrays()
 
 void solve()
 {
-	for(int i = 0;i <= newsize;i++)c[i][0] = 0;
+	for(int i = 0;i <= N;i++)c[i][0] = 0;
 	for(int l = 0;l <= length;l++)c[0][l] = 0;
 	for(int x=0;x<=length+1;x++)printf("**** ");printf("\n");
 	
-	for(int i = 1;i <= newsize;i++){
-		for(int l = 1;l <= length;l++){
-			if(ms[i].length <= l){
-				if(ms[i].price + c[i-1][l-ms[i].length] > c[i-1][l]){
-					c[i][l] = ms[i].price + c[i-1][l-ms[i].length];
-				}else{
-					c[i][l] = c[i-1][l];
-				}
+	for(int i = 0;i < N;i++){
+		for(int l = 0;l <= length;l++){
+			if(l < inputs[i].length ){
+				c[i+1][l] = c[i][l];
 			}else{
-				c[i][l] = c[i-1][l];
+				c[i+1][l] = max(c[i][l],c[i+1][l-inputs[i].length] + inputs[i].price);
 			}
 		}
+		
 	}
 }
 
@@ -100,13 +76,22 @@ void backtrack(int i, int j, int index)
 {
 	if(j < 1 || i < 1)return;
 	while(c[i--][j] == c[i][j]);
-	result[index++] = ms[++i].length;
-	backtrack(i,j-ms[i].length,index);
+	result[index++] = ++i;
+	backtrack(i,j-inputs[i-1].length,index);
+}
+
+int max(int a, int b)
+{
+	if(a>b){
+		return a;
+	}else {
+		return b;
+	}
 }
 
 void print_result()
 {
-	for(int i = -1;i <= newsize;i++){
+	for(int i = -1;i <= N;i++){
 		for(int l = -1;l <= length;l++){
 			if(i < 0 && l < 0){
 				printf("     ");
@@ -124,7 +109,7 @@ void print_result()
 	}
 	printf("\n");
 
-	printf("MAXIMUM PRICE IS [ %2d ]\n\n",c[newsize][length]);
+	printf("MAXIMUM PRICE IS [ %2d ]\n\n",c[N][length]);
 
 	printf("Cutting List = [ ");
 	for(int i = 0;i <= N;i++){
